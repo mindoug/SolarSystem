8a113d8f8cdc041d211c3a289dd66a471caed6ee
Mindy Douglas <mindy.douglas@icloud.com>
2022-04-28 10:36:00 -0500

    Initial Commit

---
+++ SolarSystem/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SolarSystem
+//
+//  Created by Mindy Douglas on 4/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

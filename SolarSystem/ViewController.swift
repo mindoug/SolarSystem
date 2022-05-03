53b9f2754facdc60fb89d15a353b0bd2bd1ab4d9
Mindy Douglas <mindy.douglas@icloud.com>
2022-05-03 08:37:06 -0500

    add text field

--- SolarSystem/ViewController.swift
+++ SolarSystem/ViewController.swift
@@ -8,10 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
+    
+    // select primary action triggered
+    @IBAction func keyboaredReturnKeyTapped(_ sender: UITextField) {
+        if let text = sender.text {
+            nameLabel.text = "Welcome, \(text)"
+        }
     }
     
 }

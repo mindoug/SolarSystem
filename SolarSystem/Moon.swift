177496dc0564d51f7669d5f2e6d810864f6b4257
Mindy Douglas <mindy.douglas@icloud.com>
2022-05-09 11:35:59 -0500

    add moon info

---
+++ SolarSystem/Moon.swift
@@ -0,0 +1,14 @@
+//
+//  Moon.swift
+//  SolarSystem
+//
+//  Created by Mindy Douglas on 5/9/22.
+//
+
+import Foundation
+
+struct Moon {
+    var info: String = """
+"It is a beautiful and delightful sight to behold the glory of the moon" - Galileo Galilei
+"""
+}

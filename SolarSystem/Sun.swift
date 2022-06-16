ef2fe4ffa6958afd5a377e4f5e756334f5c2d390
Mindy Douglas <mindy.douglas@icloud.com>
2022-06-16 13:46:47 -0500

    update sun info

--- SolarSystem/Sun.swift
+++ SolarSystem/Sun.swift
@@ -11,5 +11,7 @@ struct Sun {
     var info: String = """
 The planets and moons in our solar system are blatantly visible because they reflect sunlight.  Without the nearby Sun, these planets would be cryptic and dark on the sky.
 - Seth Shostak
+
+The Sun is a star that is 4.5 billion years old.  It is made of hydrogen and helium gasses.  Our planet relies on energy from the sun to sustain life. The Sun is the largest object in the solar system and its gravity holds the solar system together as all of the planets orbit around it. Temerpatures at the Sun's core can get as hot as 27 million degress Farenheit.
 """
 }
